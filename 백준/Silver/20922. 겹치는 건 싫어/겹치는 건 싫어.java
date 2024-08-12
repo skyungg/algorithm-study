a879c708708af25d613b49e8d2286159f288de23
허승경 <167261107+skyungg@users.noreply.github.com>
2024-08-12 17:08:20 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 308 ms, Memory: 32996 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,14 +1,12 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String [] args) throws IOException {
+    public static void main(String [] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 1. 입력 받기
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
@@ -18,24 +16,22 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 2. 연속 수열을 찾아보자
-        int start = 0;
-        int end = 0;
-        int maxLen = 0;         // 최장길이
         int [] cnt = new int[100001];
 
-        while(end < n){
-            while(end < n && cnt[arr[end]] + 1 <= k){   // 범위 내 && k번 이내일 경우
-                cnt[arr[end]] += 1;
+        int start = 0;
+        int end = 0;
+        int maxLen = 0;
+
+        while(start <= end && end < n){
+            if(cnt[arr[end]] + 1 <= k){
+                cnt[arr[end]]++;
+                maxLen = Math.max(maxLen, end-start+1);
                 end++;
+            }else{
+                cnt[arr[start]]--;
+                start++;
             }
-
-            int tmp = end - start;
-            maxLen = Math.max(maxLen, tmp);
-            cnt[arr[start]]--;
-            start++;
         }
-
         System.out.print(maxLen);
     }
 }
