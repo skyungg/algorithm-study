87418b8a35a3a9f681ea17b873d5c56a4de7b864
허승경 <167261107+skyungg@users.noreply.github.com>
2024-08-26 20:17:49 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,15 +1,18 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String [] args) throws IOException {
+    public static void main(String [] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        String result = "";
 
-        int dp [] = new int[1001];
+        if (n % 2 == 0) {
+            result = "CY";
+        } else {
+            result = "SK";
+        }
 
-        if(n%2 == 0) System.out.print("CY");
-        else System.out.print("SK");
+        System.out.println(result);
     }
 }
