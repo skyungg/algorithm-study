48efa2ac82ade759f2930c7ddbecedd017c39904
skyungg <167261107+skyungg@users.noreply.github.com>
2024-10-09 15:28:11 +0900

    [level 2] Title: [1차] 캐시, Time: 37.10 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,32 +3,26 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        ArrayList<String> cache = new ArrayList<>();
-
-        if(cacheSize == 0){ //캐시 크기가 0이면
-            return cities.length * 5;
-        }
-
-        for(int i=0;i<cities.length;i++){
-            String city = cities[i].toUpperCase(); //대소문자 구분 안하니까
-
-            if(cache.contains(city)){ //만약 있다면
-                cache.remove(city); //해당 도시 지우고
-                cache.add(city); //맨 뒤에 추가
-                answer+=1;
-            }
-
-            else{ //없다면
-                if(cache.size()==cacheSize){ //이미 캐시 꽉차있으면
-                    cache.remove(0); //맨 앞 도시 지우고
+        ArrayList<String> list = new ArrayList<>();
+        if(cacheSize == 0) return cities.length * 5;    // 캐시 적용 모하면 도시배열 길이 *5
+        
+        for(int i = 0; i < cities.length; i++){
+            String str = cities[i].toUpperCase();   // 대문자 변화
+            
+            if(list.contains(str)){
+                list.remove(str);
+                list.add(str);
+                answer += 1;
+            }else{
+                if(list.size() >= cacheSize){
+                    list.remove(0);
                 }
-
-              //캐시가 다 안찼으면
-                cache.add(city); //맨 뒤에 추가
-                answer+=5;
-            }
+                list.add(str);
+                answer += 5;
+            }  
         }
-
+        
+        
         return answer;
     }
 }
