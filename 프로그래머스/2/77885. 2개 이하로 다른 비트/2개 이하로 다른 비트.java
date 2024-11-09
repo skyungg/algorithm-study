a3a677ec4bde459e0c3dd8bc3e5979bc6bb10e36
skyungg <167261107+skyungg@users.noreply.github.com>
2024-11-10 01:24:40 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 81.01 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,25 +1,22 @@
-import java.util.*;
-
 class Solution {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
         
-        for (int i = 0; i<numbers.length; i++) {
-            long result = numbers[i];
-            String target = Long.toBinaryString(numbers[i]);
+        for(int i = 0; i < numbers.length; i++){
+            long num = numbers[i];
             
-            if (result % 2 == 0) {
-                answer[i] = result + 1;
-            } else {
-                int idx = target.lastIndexOf("0");
+            if(num%2 == 0){ // 현재 수가 짝수 -> 다음 수가 정답
+                answer[i] = num+1;
+            }else{
+                String bin_num = Long.toBinaryString(num);
                 
-                if (idx == -1) {
-                    String tmp = "10" + target.substring(1, target.length());
-                    answer[i] = Long.parseLong(tmp, 2);
-                } else {
-                    String tmp = target.substring(0, idx) + "10" + target.substring(idx + 2, target.length());
-                    answer[i] = Long.parseLong(tmp, 2);
+                if(!bin_num.contains("0")){  // 모두 0으로 이루어진 경우
+                    bin_num = "10" + bin_num.substring(1);  // 앞에 10으로 시작 + 기존 앞자리 삭제
+                }else{      // 1,0 으로 이루어진 경우
+                    int lastIndex = bin_num.lastIndexOf("0");   // 가장 오른쪽에 있는 1의 인덱스
+                    bin_num = bin_num.substring(0, lastIndex) + "10" + bin_num.substring(lastIndex + 2);
                 }
+                answer[i] = Long.parseLong(bin_num, 2);
             }
         }
         return answer;
