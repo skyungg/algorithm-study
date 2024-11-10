20fb055faead352b8e84569d0faa74c48408e975
skyungg <167261107+skyungg@users.noreply.github.com>
2024-11-10 15:02:42 +0900

    [Silver III] Title: 배열 복원하기, Time: 456 ms, Memory: 46172 KB -BaekjoonHub

--- 백준/Silver/16967. 배열 복원하기/배열 복원하기.java
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.java
@@ -5,14 +5,13 @@ public class Main {
     public static void main(String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int h = Integer.parseInt(st.nextToken());
         int w = Integer.parseInt(st.nextToken());
         int x = Integer.parseInt(st.nextToken());
         int y = Integer.parseInt(st.nextToken());
 
-        int [][] arr_a = new int[h][w];         // 배열 a
-        int [][] arr_b = new int[h+x][w+y];     // 배열 b
+        int arr_a [][] = new int[h][w];     // 배열 a
+        int arr_b [][] = new int[h+x][w+y]; // 배열 b
         for(int i = 0; i < h+x; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < w+y; j++){
@@ -20,22 +19,21 @@ public class Main {
             }
         }
 
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < h; i++){
             for(int j = 0; j < w; j++){
                 if(i >= x && j >= y){
                     arr_a[i][j] = arr_b[i][j] - arr_a[i-x][j-y];
+                    sb.append(arr_a[i][j] + " ");
                 }else{
-                    arr_a[i][j] = arr_b[i][j];
+                    arr_a[i][j] = arr_b[i][j];     // 확장 전까지는 같은 값
+                    sb.append(arr_a[i][j] + " ");
                 }
             }
+            sb.append("\n");
         }
 
-        for(int [] arr : arr_a){
-            for(int num : arr){
-                System.out.print(num+" ");
-            }
-            System.out.println();
-        }
+        System.out.println(sb);
 
     }
 }
