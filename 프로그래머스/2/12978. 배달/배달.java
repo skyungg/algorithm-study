32071b053e9b94a6e7c80fd4ac7c836917fef90c
skyungg <167261107+skyungg@users.noreply.github.com>
2024-11-13 10:04:19 +0900

    [level 2] Title: 배달, Time: 18.04 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,53 +1,40 @@
 import java.util.*;
 
 class Solution {
-    ArrayList<ArrayList<int []>> list = new ArrayList<>();
-    int times[];
-    
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        times = new int[N+1];
         
-        for(int i = 0; i < N+1; i++){
-            list.add(new ArrayList<>());
+        int [][] distance = new int[N+1][N+1];
+        
+        for(int i = 1; i <= N; i++){
+            Arrays.fill(distance[i], Integer.MAX_VALUE);    // 모든 값 -> 최댓값
+            distance[i][i] = 0; // 자기 자신은 0
         }
         
-        for(int i = 0; i < road.length; i++){
-            int a = road[i][0];
-            int b = road[i][1];
-            int c = road[i][2];
+        for(int [] r : road){
+            int a = r[0];
+            int b = r[1];
+            int c = r[2];
             
-            list.get(a).add(new int[]{b, c});
-            list.get(b).add(new int[]{a, c});
+            distance[a][b] = Math.min(distance[a][b], c);
+            distance[b][a] = Math.min(distance[b][a], c);
         }
         
-        dijkstra(N, K);
-        
-        for(int time : times){
-            if(time <= K) answer++;
-        }
-        
-        return answer;
-    }
-    
-    void dijkstra(int N, int k){
-        PriorityQueue<int []> que = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        Arrays.fill(times, Integer.MAX_VALUE);
-        times[1] = 0;
-        que.add(new int[]{1, 0});   // 현재 위치, 시간
-        
-        while(!que.isEmpty()){
-            int [] cur_point = que.poll();
-            
-            ArrayList<int []> points = list.get(cur_point[0]);
-            for(int i= 0; i < points.size(); i++){
-
-                if(cur_point[1] + points.get(i)[1] < times[points.get(i)[0]]){
-                    times[points.get(i)[0]] = cur_point[1] + points.get(i)[1];
-                    que.add(new int[]{points.get(i)[0], cur_point[1] + points.get(i)[1]});
+        // 플로이드 와샬
+        for(int k = 1; k <= N; k++){
+            for(int i = 1; i <= N; i++){
+                for(int j = 1; j <= N; j++){
+                    if (distance[i][k] != Integer.MAX_VALUE && distance[k][j] != Integer.MAX_VALUE) {
+                        distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
+                    }
                 }
             }
-            
         }
+
+        for(int i = 1; i <= N; i++){
+            if(distance[1][i] <= K) answer++;
+        }
+
+        return answer;
     }
 }
