533d3f9e82c59807bff210fe82f6e3e1905afe7c
skyungg <167261107+skyungg@users.noreply.github.com>
2024-11-13 16:46:01 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 27.84 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] orders, int[] course) {
+        ArrayList<String> result = new ArrayList<>();   // 결과
+        
+        for(int cou : course){
+            HashMap<String, Integer> map = new HashMap<>(); // <문자조합, 빈도수>
+            
+            // 크기가 cou인 조합 구하기
+            for(String order : orders){
+                char[] orderArr = order.toCharArray();
+                Arrays.sort(orderArr);
+                combination(orderArr, cou, 0, "", map);
+            }
+            
+            // 빈도수가 2이 이상인 조합에서 최빈값 구하기
+            int maxCount = 0;
+            for(int count : map.values()){
+                maxCount = Math.max(maxCount, count);
+            }
+            
+            // 최빈값인 조합 문자열 result에 추가하기
+            if(maxCount < 2) continue;
+            for(Map.Entry<String, Integer> entry : map.entrySet()){
+                if(entry.getValue() == maxCount){
+                    result.add(entry.getKey());
+                }
+            }
+        }
+        
+        String [] answer = new String[result.size()];
+        Collections.sort(result);
+        for(int i = 0; i < result.size(); i++){
+            answer[i] = result.get(i);
+        }
+        
+        
+        
+        return answer;
+    }
+    void combination(char [] order, int depth, int start, String str, HashMap<String, Integer> map){
+        if(depth == str.length()){
+            map.put(str, map.getOrDefault(str, 0)+1);
+            return;
+        }
+        
+        for(int i = start; i < order.length; i++){
+            combination(order, depth, i+1, str+order[i], map);
+        }
+    }
+}
