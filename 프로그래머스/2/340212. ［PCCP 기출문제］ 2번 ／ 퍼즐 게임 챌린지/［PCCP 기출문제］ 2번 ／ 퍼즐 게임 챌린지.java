59fac5f538b260c663a8666fe157892a5d9ce6f2
skyungg <167261107+skyungg@users.noreply.github.com>
2024-11-13 17:26:57 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 46.35 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -3,12 +3,23 @@ import java.util.*;
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
         int answer = 0;
+        int n = diffs.length;
         int start = 1;
         int end = Arrays.stream(diffs).max().getAsInt();
         
-        while(start <= end){
-            int level = (start+end)/2;
-            long total_time = calculateTime(level, diffs, times);
+        while(start <= end ){
+            int level = (start+end)/2;      // 레벨
+            long total_time = 0;
+            
+            // 시간 구하기
+            for(int i = 0; i < n; i++){
+                if(diffs[i] <= level){   // pre_cur로 해결
+                    total_time += times[i];
+                }else{
+                    // (난이도-등급)*이전시간 + 현재시간
+                    total_time += (diffs[i]-level)*(times[i-1]+times[i]) + times[i];
+                }
+            }
             
             if(total_time <= limit){
                 answer = level;
@@ -18,19 +29,6 @@ class Solution {
             }
         }
         
-    
         return answer;
     }
-    
-    long calculateTime(int middle_level, int[] diffs, int[] times){
-        long time = 0; 
-        for(int i = 0; i < diffs.length; i++){
-            if(diffs[i] <= middle_level) time += times[i];
-            else{
-                time += (diffs[i]-middle_level)*(times[i]+times[i-1]) + times[i];
-            }
-        }
-        return time;
-    }
-    
 }
