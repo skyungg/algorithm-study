3d9928936cb8acb2fc3f21d681f0ac626a2d2fa2
skyungg <167261107+skyungg@users.noreply.github.com>
2024-11-13 09:19:14 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.06 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,17 +1,17 @@
-class Solution {    
+class Solution {
     public String solution(int n) {
+        String answer = "";
         StringBuilder sb = new StringBuilder();
-        
         while(n > 0){
-            int mod = n%3;  // 3으로 나눈 나머지
-            n = n/3;    // 3으로 나눈 몫
+            int mod = n % 3;    // 3으로 나눈 나머지
+            n = n/3;    // 3으로 나눴을 때 몫
             
             if(mod == 0){
-                // 3의 배수 -> 끝자리 4
-                sb.append(4);
-                n--;
-            }else{
-                sb.append(mod);
+                sb.append(4);  // 나머지가 0일 때는 4
+                n--;    // 나누어 떨어졌을 때는 n을 1 감소
+            }
+            else{
+                sb.append(mod);     // 그 이외 나머지는 1 or 2
             }
         }
         
