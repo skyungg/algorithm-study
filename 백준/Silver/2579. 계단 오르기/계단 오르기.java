37e895e922a64f47c2acae3ad489f62e75634146
skyungg <167261107+skyungg@users.noreply.github.com>
2024-12-03 21:20:10 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int [] score = new int[n+1];
+        for(int i = 1; i <= n; i++){
+            score[i] = Integer.parseInt(br.readLine());
+        }
+
+        if (n == 1) {
+            System.out.println(score[1]);
+            return;
+        }
+        if (n == 2) {
+            System.out.println(score[1] + score[2]);
+            return;
+        }
+
+        int [] dp = new int[n+1];       // 현재 계단에서의 최댓값
+        dp[1] = score[1];
+
+        if(n >= 2){
+            dp[2] = score[1]+score[2]; // 2이상인 계단은, 다 밟는게 최댓값!
+        }
+
+        for(int i = 3; i <= n; i++){
+            dp[i] = Math.max(dp[i-2], dp[i-3]+score[i-1]) + score[i];
+        }
+
+        System.out.println(dp[n]);       // 둘 중 최댓값이 정답
+    }
+}
