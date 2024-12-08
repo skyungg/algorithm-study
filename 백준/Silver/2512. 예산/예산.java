b9d58fabb5c17d863f7bd4218c617a6e106c465c
skyungg <167261107+skyungg@users.noreply.github.com>
2024-12-08 23:22:41 +0900

    [Silver II] Title: 예산, Time: 144 ms, Memory: 15308 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.util.*;
 
@@ -6,39 +7,32 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int [] arr = new int[n];
+        int start = 0;
+        int end = -1;
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
+            end = Math.max(end, arr[i]);
         }
+
         Long m = Long.parseLong(br.readLine());
 
-        long sum = 0;
-        for(int i = 0; i < n; i++){
-            sum += arr[i];
-        }
-        long avg = sum/n;
+        int result = 0;
+        while(start <= end){
+            int mid = (start+end)/2;
 
-        int start = 1;        // 최솟값
-        int end = Arrays.stream(arr).max().getAsInt();          // 최댓값
-
-        long maxAvg = 0;
-        long result = 0;
-        while(avg >= start && avg <= end){
-            long tmp = 0;
+            long sum = 0;
             for(int i = 0; i < n ; i++){
-                if(arr[i] <= avg){
-                    tmp += arr[i];
-                }else tmp += avg;
+                if(arr[i] <= mid){
+                    sum += arr[i];
+                }else sum += mid;
             }
 
-            if(tmp <= m){
-                if(maxAvg < tmp){
-                    maxAvg = tmp;
-                    result = avg;
-                    avg++;
-                }else break;
+            if(sum <= m){
+                result = Math.max(result, mid);
+                start = mid + 1;
             }else{
-                avg--;
+                end = mid - 1;
             }
         }
 
