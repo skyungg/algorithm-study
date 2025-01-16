4274e6446ee05af7a66309705c51aea163ce40b9
skyungg <167261107+skyungg@users.noreply.github.com>
2025-01-16 19:13:40 +0900

    [level 3] Title: 기지국 설치, Time: 1.06 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,27 +1,21 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int point = 1;      // 현재 위치
-        int coverage = 2 * w + 1;   // 기지국 영향 범위
-        
-        // 전처리
-        for(int station : stations){
-            int start = station - w;    // 기지국 시작 영향권 위치
-            int end = station + w;  // 기지국 마지막 영향권 위치
-            
-            if(point  < start){
-                int empty = start - point;    // 포함 되지 않는 영역 수
-                answer += (empty+coverage-1)/coverage;
-            }
-            
-            point = end + 1;    // 위치 갱신 -> 현재 영향권의 마지막 위치 +1인 위치
+        int s=0;
+        int left =1;
+        while(true){
+        if(s < stations.length && left >= stations[s] - w){
+            left = stations[s]+w+1;
+            s++;            
         }
-        
-        // 남은 거 처리
-        if(point <= n){
-            int empty = n - point +1;
-            answer += (empty+coverage-1)/coverage;
+        else{     
+            left=left+w+w+1;
+            answer++;
         }
+        if(left> n){
+            break;
+        }
+    }
 
         return answer;
     }
