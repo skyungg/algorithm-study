6b004c10088e11a5308f4a6d3465561ee61a88cb
skyungg <167261107+skyungg@users.noreply.github.com>
2025-02-06 12:23:34 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 132 ms, Memory: 16336 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -1,26 +1,36 @@
+/*
+ * 아이디어: 구현, 그리디..?
+ * */
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String n = br.readLine();
-		int i=0;
-		while(true) {
-			i++;
-			String num=Integer.toString(i);
-			while(num.length()>0&&n.length()>0) {
-				if(num.charAt(0)==n.charAt(0)) { 
-					n=n.substring(1);
+		String nums = br.readLine();
+		int idx = 0;
+		int num = 0;
+		
+		while(idx < nums.length()) {
+			num++;		// 자연수를 1부터 시작해서 계속 증가하기
+			String str = Integer.toString(num);
+			
+			for(int i = 0; i < str.length(); i++) {
+				char ch = str.charAt(i);
+				
+				if(idx < nums.length() && ch == nums.charAt(idx)) {
+					idx++;	// 현재 문자 ch가 구해야하는 nums에 포함되어 있으면 idx 증가
 				}
-				num=num.substring(1);
-			}
-			if(n=="") {
-				System.out.println(i);
-				break;
+				if(idx >= nums.length()) break;	// 다 찾음
 			}
 			
 		}
+		
+		System.out.println(num);
+		
 	}
 
 }
