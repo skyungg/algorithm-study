08de5e374f47caef09c3bf7aa5c477e189cf699d
skyungg <167261107+skyungg@users.noreply.github.com>
2025-02-23 00:03:38 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 124 ms, Memory: 16300 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -1,37 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String [] args)throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int [][] dp = new int[10001][4];
 
-        // 합이 3이하인 n은 초기값 1 (n = 1, 2, 3)
-        dp[1][1] = 1;
-        dp[2][1] = 1;
-        dp[2][2] = 1;
-        dp[3][1] = 1;
-        dp[3][2] = 1;
-        dp[3][3] = 1;
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		int [][] dp = new int[10001][3];
+		dp[1][0] = dp[2][0] = dp[2][1] = dp[3][0] = dp[3][1] = dp[3][2] = 1;
+		
+		for(int i = 4; i < 10001; i++) {
+			dp[i][0] = dp[i-1][0];
+			dp[i][1] = dp[i-2][0] + dp[i-2][1];
+			dp[i][2] = dp[i-3][0] + dp[i-3][1] + dp[i-3][2];
+			
+		}
+		
+		for(int tc = 0; tc < t; tc++) {
+			int n = Integer.parseInt(br.readLine());
+			int result = dp[n][0] + dp[n][1] + dp[n][2];
+			sb.append(result+"\n");
+		}
+		
+		System.out.println(sb.toString());
 
-        for(int i = 4; i < 10001; i++){
-            dp[i][1] = dp[i-1][1];
-            dp[i][2] = dp[i-2][1] + dp[i-2][2];
-            dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
-        }
+	}
 
-        int t = Integer.parseInt(br.readLine());
-
-        for(int tc = 0; tc < t; tc++){
-            int n = Integer.parseInt(br.readLine());
-
-            int res = dp[n][1] + dp[n][2] + dp[n][3];
-            sb.append(res+"\n");
-        }
-
-        System.out.print(sb);
-
-    }
 }
