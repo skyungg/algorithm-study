1bc904f9dddbd72cabf162db2cfc658d9318f255
skyungg <167261107+skyungg@users.noreply.github.com>
2025-03-06 11:02:45 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.41 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,56 +1,52 @@
 import java.util.*;
 
 class Solution {
-    static int [][] jobs;
-    static class Point implements Comparable <Point>{
+    class Point implements Comparable<Point> {
+        int startAt;
         int time;
-        int start;
         
-        public Point(int time, int start){
+        public Point(int startAt, int time){
+            this.startAt = startAt;
             this.time = time;
-            this.start = start;
         }
         
         @Override
         public int compareTo(Point p){
-            if(this.time == p.time) return this.start - p.start;
-            
+            // 우선순위 소요시간 -> 요청 시각
+            if(this.time == p.time) return this.startAt - p.startAt;
             return this.time - p.time;
         }
-    }
-    
-    public int solution(int[][] jobs) {
-        this.jobs = jobs;
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
-        int answer = bfs();
-        return answer;
-    }
-    
-    int bfs(){
-        PriorityQueue<Point> pq = new PriorityQueue<>();
-        int totalTime = 0;
-        int currentTime = 0;    // 현재 시간
-        int index = 0;          // jobs 배열의 인덱스
-        int n = jobs.length;
         
-        while(index < n || !pq.isEmpty()){
-            
-            while(index < n && jobs[index][0] <= currentTime){
-                pq.add(new Point(jobs[index][1], jobs[index][0]));
-                index++;
+        
+    }
+    public int solution(int[][] jobs) {
+        // 1. 요청시각 기준 오름차순 정렬
+        Arrays.sort(jobs, (a, b) -> a[0]-b[0]);
+        
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        
+        int n = jobs.length;
+        int idx = 0;      // 진행하고 있는 순서
+        int endAt = 0;    // 현재 시각
+        int count = 0;    // 완료된 작업 수
+        int total = 0;    // 총 소요시간 
+        
+        while(count < n){
+            while(idx < n && jobs[idx][0] <= endAt){
+                pq.add(new Point(jobs[idx][0], jobs[idx][1]));
+                idx++;
             }
             
             if(pq.isEmpty()){
-                currentTime = jobs[index][0];
+                endAt = jobs[idx][0];
             }else{
                 Point p = pq.poll();
-                currentTime += p.time;
-                totalTime += currentTime - p.start;
+                endAt += p.time;    // 작업 끝나는 시간 갱신
+                total += (endAt - p.startAt);       //요청 시각 ~ 작업 끝나는 시각
+                count++;
             }
         }
         
-        return totalTime/n;
+        return total/n;
     }
-    
-    
 }
