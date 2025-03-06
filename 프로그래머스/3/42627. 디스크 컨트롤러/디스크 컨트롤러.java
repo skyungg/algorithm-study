fe8a08aaf7757aa67f769d76392f2dc00be84c8a
skyungg <167261107+skyungg@users.noreply.github.com>
2025-03-06 11:08:13 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.94 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -26,23 +26,26 @@ class Solution {
         PriorityQueue<Point> pq = new PriorityQueue<>();
         
         int n = jobs.length;
-        int idx = 0;      // 진행하고 있는 순서
-        int endAt = 0;    // 현재 시각
+        int idx = 0;      
+        int endAt = 0;    // 현재 시간
+        int total = 0;    // 총 대기시간
         int count = 0;    // 완료된 작업 수
-        int total = 0;    // 총 소요시간 
         
         while(count < n){
+            // 현재 시간 이전에 요청된 작업들 pq에 추가
             while(idx < n && jobs[idx][0] <= endAt){
                 pq.add(new Point(jobs[idx][0], jobs[idx][1]));
                 idx++;
             }
             
             if(pq.isEmpty()){
+                // 큐가 비어있을 경우 -> 다른 요청 들어오는 시간으로 갱신
                 endAt = jobs[idx][0];
             }else{
+                // pq에서 실행시간 가장 짧은 작업 수행
                 Point p = pq.poll();
                 endAt += p.time;    // 작업 끝나는 시간 갱신
-                total += (endAt - p.startAt);       //요청 시각 ~ 작업 끝나는 시각
+                total += (endAt - p.startAt);       //요청 시간 부터 종료까지 걸린 시간
                 count++;
             }
         }
