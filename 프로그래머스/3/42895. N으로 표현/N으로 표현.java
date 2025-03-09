6a225775369887c453dc24671207365d841bd49d
skyungg <167261107+skyungg@users.noreply.github.com>
2025-03-09 12:04:20 +0900

    [level 3] Title: N으로 표현, Time: 36.42 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,6 +1,17 @@
 import java.util.*;
 
-// 
+/*
+문제 이해
+1. N을 이용해서 number 만들기
+2. N의 최소 사용횟수를 구하는게 목표!
+3. N은 최대 8번까지 사용 가능
+4. 사칙연산 통해서 새로운 수 만들수 있음
+
+* dp[i]가 의미하는 건?*
+--> N을 i번 사용해서 만들 수 있는 숫자들의 집합
+
+
+*/
 class Solution {
     public int solution(int N, int number) {
         int answer = 0;
