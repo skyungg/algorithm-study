e1c4abc6cf58579c34427b95ae57157ccc278951
skyungg <167261107+skyungg@users.noreply.github.com>
2025-03-27 14:41:59 +0900

    [level 3] Title: 불량 사용자, Time: 119.25 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,40 +1,67 @@
 import java.util.*;
 
+/*
+아이디어: 일단 구현
+1. b_id에 해당하는 u_id를 list에 저장하기 -> List<List<String>> -> 어차피 순서대로 하니까 map 안씀
+2. 위의 list 가지고 순열조합 만드고, set에 넣기
+3. set.size()가 정답
+*/
+
 class Solution {
+    List<List<String>> list = new ArrayList<>();
+    Set<Set<String>> set = new HashSet<>();
     
     public int solution(String[] user_id, String[] banned_id) {
-        Set<Set<String>> set = new HashSet<>();
         
-        // 백트래킹
-        backtrack(user_id, banned_id, 0, new HashSet<>(), set);
-        
-        return set.size();
-    }
-    
-    void backtrack(String[] user_id, String[] banned_id, int idx, Set<String> curSet, Set<Set<String>> set){
-        if(idx == banned_id.length){
-            set.add(new HashSet(curSet));   // 현재 조합 추가
-            return;
+        for(int i = 0; i < banned_id.length; i++){
+            list.add(new ArrayList<>());
         }
         
-        String bId = banned_id[idx];
-        for(String user: user_id){
-            if(!curSet.contains(user) && isMatch(user, bId)){
-                curSet.add(user); // 유저의 아이디를 현재 조합에 추가
-                backtrack(user_id, banned_id, idx+1, curSet, set);
-                curSet.remove(user);
+        // 1. b_id에 해당하는 u_id의 리스트 
+        for(int idx = 0; idx < banned_id.length; idx++){
+            String target = banned_id[idx];
+            
+            for(String str : user_id){
+                if(str.length() == target.length()){
+                    boolean flag = true;
+                    
+                    for(int j = 0; j < target.length(); j++){
+                        if(target.charAt(j) == '*') continue;
+                        if(target.charAt(j) != str.charAt(j)){
+                            flag = false;
+                            break;
+                         }
+                    }
+                    
+                    if(flag){
+                        list.get(idx).add(str);
+                    }
+                }
             }
         }
+        
+        // 2. 순열 조합 구하기
+        backtracking(0, new HashSet<>());
+        
+        // 3. 정답 출력
+        return set.size();
+        
     }
     
-    boolean isMatch(String user, String b_id){
-        if(user.length() != b_id.length()) return false;    // 길이 다르면 비교 불가
-        
-        // 문자별로 비교
-        for(int i = 0; i < user.length(); i++){
-            if(b_id.charAt(i) != '*' && user.charAt(i) != b_id.charAt(i)) return false;
+    void backtracking(int depth, Set<String> curSet){
+        // 가지치기
+        if(depth == list.size()){        // 하나 조합 만들어진 경우
+            set.add(new HashSet<>(curSet));
+            return;   
         }
         
-        return true;
+        
+        for(String tmp : list.get(depth)){
+            if(curSet.contains(tmp)) continue;
+            
+            curSet.add(tmp);
+            backtracking(depth+1, curSet);
+            curSet.remove(tmp);
+        }
     }
 }
