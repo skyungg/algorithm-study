bfdbae9faa23837c309532e99ad473b0c0341614
skyungg <167261107+skyungg@users.noreply.github.com>
2025-03-27 13:09:27 +0900

    [Gold V] Title: 택배 배송, Time: 412 ms, Memory: 40148 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -1,76 +1,83 @@
 import java.io.*;
 import java.util.*;
 
+/*
+ * 아이디어: 다익스트라
+ * 이유: 간선마다 값이 다르고, 하나의 노드에서 특정노드로 가야함
+ * [구현]
+ * 1. (num, cost) 원소로하는 Node 클래스 생성
+ * 2. 1번에서 i번노드까지 가는데 최소 여물값 저장하는 costArr 생성
+ * 3. 1번 노드부터 bfs 돌려~~
+ * 4. 현재 노드의 cost + 인접한 노드의 cost가 인접 노드 costArr 보다 작으면 갱신
+ * 5. 최종으로는 costArr에서 n번째 인덱스 값 출력
+ * */
+
 public class Main {
-	static class Point implements Comparable<Point>{
+	static class Node implements Comparable<Node>{
 		int num;
-		int w;
+		int cost;
 		
-		public Point(int num, int w) {
+		public Node(int num, int cost) {
 			this.num = num;
-			this.w = w;
+			this.cost = cost;
 		}
 		
 		@Override
-		public int compareTo(Point p) {
-			return this.w - p.w;	// 소의 수 오름차순 정렬
+		public int compareTo(Node n) {	// cost 기준 오름차순 정렬
+			return this.cost - n.cost;
 		}
 	}
-	
 	static int n, m;
-	static List<List<Point>> map = new ArrayList<>();
-	
+	static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());	// 정점 개수
+		m = Integer.parseInt(st.nextToken());	// 간선의 개수
 		
 		for(int i = 0; i < n+1; i++) {
-			map.add(new ArrayList<>());
+			graph.add(new ArrayList<>());
 		}
 		
 		for(int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			
-			map.get(a).add(new Point(b, c));
-			map.get(b).add(new Point(a, c));
+			graph.get(a).add(new Node(b, c));		// 인접 그래프
+			graph.get(b).add(new Node(a, c));
 		}
 		
-		// 다익스트라
-		int answer = dijkstra();
+		// bfs 구현
+		int result = dijkstra();
+		System.out.println(result);
 		
-		System.out.println(answer);
 	}
 	
 	static int dijkstra() {
-		PriorityQueue<Point> pq = new PriorityQueue<>();
-		int minCount = Integer.MAX_VALUE;		// 최소 여물의 비용
-		pq.add(new Point(1, 0));
-		int [] minWeight = new int[n+1];
-		Arrays.fill(minWeight, Integer.MAX_VALUE);
+		int [] costArr = new int[n+1];		// 1-> 각 정점으로의 여물 최솟값 저장 배열
+		Arrays.fill(costArr, Integer.MAX_VALUE);	// 초기값(최댓값으로 설정)
+		costArr[1] = 0;	// 자기 자신은 0
+		
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(1, 0));
 		
 		while(!pq.isEmpty()) {
-			Point p = pq.poll();
+			Node node = pq.poll();
 			
-			for(int i = 0; i < map.get(p.num).size(); i++) {
-				Point curPoint = map.get(p.num).get(i);
-				
-				int curWeight =curPoint.w + p.w;
-				if(curWeight < minWeight[curPoint.num]) {
-					pq.add(new Point(curPoint.num, curWeight));
-					minWeight[curPoint.num] = curWeight;
+			ArrayList<Node> list = graph.get(node.num);
+			for(int i = 0; i < list.size(); i++) {
+				if(list.get(i).cost + costArr[node.num] < costArr[list.get(i).num]) {
+					costArr[list.get(i).num] = list.get(i).cost+node.cost;
+					pq.add(new Node(list.get(i).num, costArr[list.get(i).num]));
 					
 				}
-			}			
+			}
 		}
 		
-		return minWeight[n];
+		return costArr[n];
+		
 	}
 }
