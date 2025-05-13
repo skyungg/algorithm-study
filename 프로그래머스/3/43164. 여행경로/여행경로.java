12629d88fc4f791b6e202fea049780012dba3626
skyungg <167261107+skyungg@users.noreply.github.com>
2025-05-13 14:57:37 +0900

    [level 3] Title: 여행경로, Time: 70.62 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,27 +1,34 @@
 import java.util.*;
 
 class Solution {
-    static HashMap<String, PriorityQueue<String>> hmap = new HashMap<>();
-    static LinkedList<String> route = new LinkedList<>();  // 경로 저장
+    List<String> route = new ArrayList<>();
+    boolean[] visited;
     
     public String[] solution(String[][] tickets) {
-        for(int i = 0; i < tickets.length; i++){
-            hmap.putIfAbsent(tickets[i][0], new PriorityQueue<>());
-            hmap.get(tickets[i][0]).offer(tickets[i][1]);
-        }
+        // 1. 도착지 기준 정렬하기
+        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
         
-        dfs("ICN");
-        
-        
-        return route.toArray(new String[route.size()]);
+        visited = new boolean[tickets.length];  // 티켓 사용 여부
+        // 2. 탐색하기       
+        dfs("ICN", "ICN", 0, tickets);  // 현재 도착지 및 다음 출발지, 현재 경로, 탐색 수, 티켓 배열
+
+        String [] answer = route.get(0).split(" ");
+        return answer;
     }
     
-    void dfs(String key){
-        while(hmap.containsKey(key) && !hmap.get(key).isEmpty()){
-            String nextKey = hmap.get(key).poll();
-            dfs(nextKey);
+    public void dfs(String current, String path, int count, String[][] tickets) {
+        if (count == tickets.length) {
+            // 모든 경로 방문
+            route.add(path);
+            return;
         }
-        route.addFirst(key);
-        
+
+        for (int i = 0; i < tickets.length; i++) {
+            if (!visited[i] && tickets[i][0].equals(current)) {
+                visited[i] = true;
+                dfs(tickets[i][1], path + " " + tickets[i][1], count + 1, tickets);
+                visited[i] = false;
+            }
+        }
     }
 }
