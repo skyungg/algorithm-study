acc94463dabb378c175f1ab4fb5109b59d8753ac
skyungg <167261107+skyungg@users.noreply.github.com>
2025-05-15 15:40:35 +0900

    [level 3] Title: 입국심사, Time: 138.56 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,28 +1,38 @@
 import java.util.*;
 
+/*
+아이디어: 시간을 이분탐색
+
+*/
 class Solution {
     public long solution(int n, int[] times) {
-        Arrays.sort(times);     // 입국 심사 오름차순
-        long end = (long) times[times.length - 1] * n;   // 최대 시간
-        long start = 1;     // 최소 시간
-        long answer = end;
+        long left = 1;
+        long right = 0;
+        for(int time : times){
+            if((long)time*n > right) right = (long)time*n;  // 제일 오래 걸리는 심사관으로 설정
+        }
+        long answer = right;    // 큰 값으로 초기 설정
         
-        while(start <= end){
-            long mid_time = (start + end) / 2;
+        Arrays.sort(times);
+        
+        long mid = 0;
+        while(left <= right){
+            mid = (left+right)/2;
             
-            long num = 0;   // mid_time안에 각각 심사하는 사람 수
+            long cnt = 0;    // mid 안에 심사받는 사람 수
             for(int i = 0; i < times.length; i++){
-                num += mid_time/times[i];
+                cnt += (mid/times[i]);  // mid시간 동안, times[i]로 심사 가능한 사람 수
                 
-                if(num >= n) break;
             }
             
-            if(num >= n){
-                answer = Math.min(answer, mid_time);
-                end = mid_time - 1;
-            }else start = mid_time + 1;
-        }
-        
+            if(cnt >= n){
+                answer = mid;
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }  
+
         return answer;
     }
 }
