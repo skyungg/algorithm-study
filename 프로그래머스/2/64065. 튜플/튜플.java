b7499da070dd9e86e2ff03445a5a90236e431c70
skyungg <167261107+skyungg@users.noreply.github.com>
2025-05-19 17:58:17 +0900

    [level 2] Title: 튜플, Time: 90.29 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,22 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        s = s.substring(2, s.length()-2);
-        s = s.replace("},{", "-");
-        String [] tmp = s.split("-");
-        Arrays.sort(tmp, (a, b) -> (a.length()-b.length()));    // 나눠진 문자열 길이별로 정렬
-        ArrayList<Integer> list = new ArrayList<>();
-        for(String str : tmp){
-            String [] temp = str.split(",");
-            for(int i = 0; i < temp.length; i++){
-                int num = Integer.parseInt(temp[i]);
+        
+        
+        String tmp = s.substring(2, s.length()-2);
+        
+        String [] arr = tmp.split("\\},\\{");
+        Arrays.sort(arr, (a, b) -> a.length() - b.length());
+        List<Integer> list = new ArrayList<>();
+        
+        for(String ar : arr){
+            String [] str = ar.split(",");
+            for(String st : str){
+                int num = Integer.parseInt(st);
                 if(!list.contains(num)) list.add(num);
             }
+            
         }
-
-        int answer [] = new int[list.size()];
-        for(int i = 0; i<answer.length; i++){
-            answer[i] = list.get(i);
+        
+        int [] answer = new int[list.size()];
+        int cnt = 0;
+        
+        for(int num : list){
+            answer[cnt++] = num;
         }
         
         return answer;
