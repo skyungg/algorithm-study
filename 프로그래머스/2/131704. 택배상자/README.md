d43f54ab4134980974c3f980c64f4981e320f35b
skyungg <167261107+skyungg@users.noreply.github.com>
2025-05-23 11:38:21 +0900

    [level 2] Title: 택배상자, Time: 46.68 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/README.md
+++ 프로그래머스/2/131704. 택배상자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배상자 - 131704 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704#) 
 
 ### 성능 요약
 
-메모리: 125 MB, 시간: 142.53 ms
+메모리: 154 MB, 시간: 46.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 12일 15:15:16
+2025년 05월 23일 11:38:18
 
 ### 문제 설명
 
--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -3,21 +3,31 @@ import java.util.*;
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
-        Stack<Integer> stack = new Stack<>();
-        int idx = 0;
+        int boxIdx = 1;    //  상자 순서
+        int orderIdx = 0; // 박스 순서
+        Stack<Integer> stack = new Stack<>();       // 보조 컨베이어 벨트
         
-        for(int i = 0 ; i < order.length; i++){
-            stack.add(i+1);
-            
-            while(!stack.isEmpty()){
-                if(stack.peek() == order[idx]){
+        
+        while(orderIdx < order.length){
+            if(order[orderIdx] == boxIdx){
+                answer++;
+                orderIdx++;
+                boxIdx++;
+            }else{
+                // 1. 보조 컨베이어 벨트에 추가하기
+                while(boxIdx <= order[orderIdx]){
+                    stack.add(boxIdx++);
+                }
+                
+                // 2. 보조 컨베이어 벨트의 마지막 삽입값과 일치 여부 확인
+                if(stack.peek() == order[orderIdx]){
                     stack.pop();
                     answer++;
-                    idx++;
+                    orderIdx++;
                 }else break;
             }
         }
-        
+
         return answer;
     }
 }
