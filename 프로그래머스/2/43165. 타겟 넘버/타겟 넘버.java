c1d3fb321c919329fd5cb83a1f52f41c1bd3fbf8
skyungg <167261107+skyungg@users.noreply.github.com>
2025-07-28 17:42:08 +0900

    [level 2] Title: 타겟 넘버, Time: 6.58 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -4,22 +4,26 @@
 2) 한 번은 -
 */
 class Solution {
+    int[] numbers;
+    int target;
     int count = 0;      // 방법의 수
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0);
+        this.numbers = numbers;
+        this.target = target;
+        dfs(0, 0);
         
         return count;
     }
     
-    void dfs(int [] numbers, int target, int idx, int sum){
+    void dfs(int idx, int sum){
         if(idx == numbers.length){  // 끝까지 다 왔을 때
             if(target == sum) count++;  // target 넘버랑 최종 sum이 같으면 -> 방법의 수 증가
             return; 
         }
         
         // 더하기
-        dfs(numbers, target, idx+1, sum+numbers[idx]);
+        dfs(idx+1, sum+numbers[idx]);
         // 빼기
-        dfs(numbers, target, idx+1, sum-numbers[idx]);
+        dfs(idx+1, sum-numbers[idx]);
     }
 }
