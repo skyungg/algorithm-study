5a49fdc01f302988f0bda98d84f4aed0207c95b9
skyungg <167261107+skyungg@users.noreply.github.com>
2025-08-10 10:15:38 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14652 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,65 +1,62 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
-    static class Point{
-        int x;
-        int y;
 
-        public Point(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-    static int n, m;
-    static int [][] map;
-    static boolean [][] visited;
-    static int [] dx = {-1, 0, 1, 0};
-    static int [] dy = {0, 1, 0, -1};
-    public static void main(String [] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        map = new int[n][m];
-        visited = new boolean[n][m];
-
-        for(int i = 0; i < n; i++){
-            String str = br.readLine();
-            for(int j = 0; j < m; j++){
-                map[i][j] = Integer.parseInt(Character.toString(str.charAt(j)));
-            }
-        }
-        bfs();
-
-        System.out.println(map[n-1][m-1]);
-    }
-
-    static void bfs(){
-        Queue<Point> que = new LinkedList<>();
-        que.add(new Point(0, 0));
-        visited[0][0] = true;
-
-        while(!que.isEmpty()){
-            Point p = que.poll();
-
-            for(int k = 0; k <4; k++){
-                int tx = p.x + dx[k];
-                int ty = p.y + dy[k];
-
-                if(tx >= 0 && tx < n && ty >= 0 && ty < m){
-                    if(!visited[tx][ty] && map[tx][ty] == 1){
-                        map[tx][ty] = map[p.x][p.y]+1;
-                        if(tx == n-1 && ty == m-1) return;
-                        visited[tx][ty] = true;
-                        que.add(new Point(tx, ty));
-                    }
-                }
-            }
-        }
-    }
-
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		char [][] arr = new char[N][M];
+		
+		// 입력 받기
+		for(int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for(int j = 0; j < M; j++) {
+				arr[i][j] = str.charAt(j);
+			}
+		}
+		
+		System.out.println(bfs(arr, N, M));
+	
+	}
+	
+	static int bfs(char [][] arr, int n, int m) {
+		int result = 0;
+		Queue<int []> que = new LinkedList<>();
+		boolean [][] visited = new boolean[n][m];
+		
+		que.add(new int [] {0, 0, 1});		// 초기 위치 및 누적칸 수
+		visited[0][0] = true;
+		
+		int [] dx = {-1, 0, 1, 0};
+		int [] dy = {0, 1, 0, -1};
+		
+		while(!que.isEmpty()) {
+			int [] point = que.poll();
+			
+			if(point[0] == n-1 && point[1] == m-1) {	// 목적지 도착
+				result = point[2];
+				break;
+			}
+			
+			for(int i = 0; i < 4; i++) {
+				int tx = point[0] + dx[i];
+				int ty = point[1] + dy[i];
+				
+				if(tx >= 0 && tx < n && ty >= 0 && ty < m) {
+					if(!visited[tx][ty] && arr[tx][ty] == '1') {
+						visited[tx][ty] = true;	// 방문처리
+						que.add(new int[] {tx, ty, point[2]+1});
+					}
+				}
+			}
+		}
+		
+		return result;
+	}
 }
