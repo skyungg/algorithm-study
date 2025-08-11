d85d5ab453286112c89d40faab30c5bce3dd1401
skyungg <167261107+skyungg@users.noreply.github.com>
2025-08-11 21:11:52 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,55 +1,58 @@
-/*
-7
-6
-1 2
-2 3
-1 5
-5 2
-5 6
-4 7
- */
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String [] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i <= n; i++){
-            graph.add(new ArrayList<>());
-        }
 
-        for(int i = 0; i < m; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            graph.get(a).add(b);
-            graph.get(b).add(a);
-        }
-
-        bfs(1, graph, new boolean[n+1], n);
-
-    }
-
-    static void bfs(int start, ArrayList<ArrayList<Integer>> graph, boolean [] visited, int n){
-        visited[start] = true;
-        Queue<Integer> que = new LinkedList<>();
-        que.add(start);
-        int count = 0;
-
-        while(!que.isEmpty()){
-            int num = que.poll();
-
-            for(int i = 0; i < graph.get(num).size(); i++){
-                if(visited[graph.get(num).get(i)]) continue;
-                count++;
-                visited[graph.get(num).get(i)] = true;
-                que.add(graph.get(num).get(i));
-            }
-        }
-        System.out.println(count);
-    }
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		List<List<Integer>> graph = new ArrayList<>();
+		for(int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
+		}
+		
+		for(int i = 0; i < M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			graph.get(a).add(b);
+			graph.get(b).add(a);
+		}
+		
+		System.out.println(bfs(graph, N));
+		
+	}
+	
+	static int bfs(List<List<Integer>> graph, int n) {
+		Queue<Integer> que = new LinkedList<>();
+		boolean [] visited = new boolean[n+1];
+		que.add(1);	// 1부터 시작
+		visited[1] = true;
+		
+		int result = 0;
+		
+		while(!que.isEmpty()) {
+			int target = que.poll();
+			
+			if(graph.get(target).size() > 0) {
+				List<Integer> tmpGraph = graph.get(target);
+				for(int i = 0; i < tmpGraph.size(); i++) {
+					int num = tmpGraph.get(i);
+					if(!visited[num]) {
+						visited[num] = true;
+						que.add(num);
+						result++;
+					}
+				}
+			}
+		}
+		
+		return result;
+		
+	}
 }
