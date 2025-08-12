9aa82802426e8ac70274d0551f3529fdc2e20b82
skyungg <167261107+skyungg@users.noreply.github.com>
2025-08-12 09:53:23 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 18940 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,41 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());		// 수빈이 위치
-		int k = Integer.parseInt(st.nextToken());		// 동생 위치
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 		
-		if(n == k) {
-			System.out.println(0);
-			return;
-		}
-		
-		boolean [] visited = new boolean[100001];
 		Queue<int []> que = new LinkedList<>();
-		que.add(new int[] {n, 0});
-		
+		que.add(new int [] {N, 0});		// 현재 위치, 시간
+		boolean [] visited = new boolean[100001];
+		visited[N] = true;
+		int result = 0;
 		while(!que.isEmpty()) {
-			int [] p = que.poll();
-			int point = p[0];
-			int time = p[1];
+			int [] point = que.poll();
 			
-			if(point == k) {
-				System.out.println(time);
-				return;
+			if(point[0] == K) {
+				result = point[1];
+				break;
 			}
 			
-			int [] nextPoints = {point-1, point+1, point*2};
+			// 계산
+			int ax = point[0]+1;
+			int bx = point[0]-1;
+			int cx = point[0]*2;
 			
-			for(int next : nextPoints) {
-				if(next >= 0 && next <= 100000 && !visited[next]) {
-					visited[next] = true;
-					que.add(new int[] {next, time+1});
-				}
+			if(ax >= 0 && ax <= 100000 && !visited[ax]) {
+				que.add(new int[] {ax, point[1]+1});
+				visited[ax] = true;
 			}
+			if(bx >= 0 && bx <= 100000&& !visited[bx]) {
+				que.add(new int[] {bx, point[1]+1});
+				visited[bx] = true;
+			}
+			if(cx >= 0 && cx <= 100000&& !visited[cx]) {
+				que.add(new int[] {cx, point[1]+1});
+				visited[cx] = true;
+			}
+			
 		}
+		
+		System.out.println(result);
 	}
 
 }
