93ebb66f1b1d798bda4b0e7a01cffd1268937a36
skyungg <167261107+skyungg@users.noreply.github.com>
2025-08-13 11:46:04 +0900

    [Silver I] Title: 안전 영역, Time: 264 ms, Memory: 55888 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -2,62 +2,69 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int maxCount = 1;
-    public static void main(String [] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int [][] map = new int[n][n];
-        int minH = Integer.MAX_VALUE;
-        int maxH = Integer.MIN_VALUE;
-        for(int i = 0; i < n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < n; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-                minH = Math.min(minH, map[i][j]);
-                maxH = Math.max(maxH, map[i][j]);
-            }
-        }
+	static int N;
+	static boolean [][] visited;
+	static int [][] map;
+	static int [] dx = {-1, 0, 1, 0};
+	static int [] dy = {0, 1, 0, -1};
+	
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		int maxH = 0;
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				maxH = Math.max(maxH, map[i][j]);
+			}
+		}
+		
+		int result = 0;		// 최대 안전 영역 수
+		
+		for(int h = -1; h <= maxH; h++) {
+			visited = new boolean[N][N];	// 각 높이별 방문 배열 생성
+			int tmpSum = 0;		// 현재 높이에서 안전 영역 수
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(!visited[i][j] && map[i][j] > h) {
+						bfs(h, i, j);
+						tmpSum++;
+					}
+				}
+			}
+			
+			result = Math.max(result, tmpSum);
+		}
+		
+		System.out.println(result);
+		
+	}
+	
+	static void bfs(int h, int x, int y) {
+		Queue<int []> que = new LinkedList<>();
+		que.add(new int[] {x, y});
+		visited[x][y] = true;
+		
+		while(!que.isEmpty()) {
+			int [] point = que.poll();
+			
+			for(int i = 0; i < 4; i++) {
+				int tx = point[0] + dx[i];
+				int ty = point[1] + dy[i];
+				
+				if(tx >= 0 && tx < N && ty >= 0 && ty < N) {
+					if(!visited[tx][ty] && map[tx][ty] > h) {
+						visited[tx][ty] = true;
+						que.add(new int[] {tx, ty});
+					}
+				}
+			}
+		}
 
-        boolean [][] visited = new boolean [n][n];
-        for(int h = minH; h <= maxH; h++){
-            for (boolean[] row : visited) {
-                Arrays.fill(row, false); // 초기화
-            }
-            int count = 0;
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    if(map[i][j] > h && !visited[i][j]){
-                        bfs(h, map, visited, n, i, j);
-                        count++;
-                    }
-                }
-            }
-            maxCount = Math.max(maxCount, count);
-        }
-        System.out.println(maxCount);
-    }
-
-    static void bfs(int h, int [][] map, boolean [][] visited, int n, int x, int y){
-        Queue<int []> que = new LinkedList<>();
-        que.add(new int[]{x, y});
-        visited[x][y] = true;
-
-        int [] dx = {0, -1, 0, 1};
-        int [] dy = {-1, 0, 1, 0};
-
-        while(!que.isEmpty()){
-            int [] tmp = que.poll();
-
-            for(int i = 0; i < 4; i++){
-                int tx = tmp[0] + dx[i];
-                int ty = tmp[1] + dy[i];
-
-                if(tx < 0 || tx >= n || ty < 0 || ty >= n) continue;
-                if(!visited[tx][ty] && map[tx][ty] > h){
-                    visited[tx][ty] = true;
-                    que.add(new int[]{tx, ty});
-                }
-            }
-        }
-    }
+	}
+	
 }
