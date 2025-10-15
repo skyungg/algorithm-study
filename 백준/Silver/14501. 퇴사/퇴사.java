91baf03f6a0530dec36d2692a8593fb561dfffe7
skyungg <167261107+skyungg@users.noreply.github.com>
2025-10-16 01:11:08 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,28 +1,45 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
+/*
+ * (구) 백준이가 얻을 수 있는 최대 이익
+ * */
 
 public class Main {
-    public static void main(String [] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
-        int [][] arr = new int[n][2];
-        StringTokenizer st;
-        for(int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());        // 상담 걸리는 기간
-            arr[i][1] = Integer.parseInt(st.nextToken());        // 받을 수 있는 금액
-        }
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int [] T = new int[N+1];	// 상담 기간
+		int [] P = new int[N+1];	// 상담 비용
+		
+		StringTokenizer st;
+		for(int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int [] dp = new int[N+2];	//  N일에 하루만해서 상담 끝냈을 때 N+1로 계산되니까
+		for(int i = 1; i <= N; i++) {
+			// 오늘 상담을 안 하는 경우 
+			dp[i] = Math.max(dp[i], dp[i-1]);
+			
+			// 오늘 상담함
+			if(T[i]+i <= N+1) {			// 오늘 상담했을 때, 퇴사일까지 상담 가능
+				dp[T[i]+i] = Math.max(dp[T[i]+i], dp[i]+P[i]);
+			}
+		}
+		
+		int count = 0;
+		for(int result : dp) {
+			count = Math.max(count, result);
+		}
+		
+		System.out.println(count);
+	}
 
-        int [] dp = new int[n+1];
-        for(int i = 0; i < n; i++){
-            if(i+arr[i][0] <= n){       // 날짜가 초과되지 않았을 경우
-                dp[i + arr[i][0]] = Math.max(dp[i + arr[i][0]], dp[i] + arr[i][1]);
-            }
-            dp[i+1] = Math.max(dp[i+1],dp[i]);
-        }
-
-        System.out.println(dp[n]);
-    }
 }
