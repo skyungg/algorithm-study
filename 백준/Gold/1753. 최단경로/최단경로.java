5fa5b1c9713e9e6bfc714e031db88594512e1d03
skyungg <167261107+skyungg@users.noreply.github.com>
2025-10-18 01:52:57 +0900

    [Gold IV] Title: 최단경로, Time: 696 ms, Memory: 108936 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,83 +1,89 @@
 import java.io.*;
 import java.util.*;
 
+/*
+ * (구) k위치에서 모든 정점 방문하는데 최단 경로값
+ * 아이디어: 다익스트라
+ * 
+ * */
 public class Main {
-	static class Point implements Comparable<Point>{
-		int v;
-		int w;
-		
-		public Point(int v, int w) {
-			this.v = v;
-			this.w = w;
-		}
-		
-		@Override
-		public int compareTo(Point p) {
-			return this.w - p.w;		// 오름차순 정렬
-		}
-	}
+	static class Point implements Comparable<Point> {
+	    int v;  // 도착 정점
+	    int w;  // 가중치
 
+	    public Point(int v, int w) {
+	    	this.v = v;
+	    	this.w = w;
+	    }
+
+	    @Override
+	    public int compareTo(Point p) {
+	    	return this.w - p.w;		// 올므차순 정렬
+	    }
+	}
+	
+	static int V, E;
+	static List<List<Point>> graph = new ArrayList<>();		// 인접 배열
+	static int [] dist;			// 각 정점가지의 최단거리
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int V = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(br.readLine());
-		
-		ArrayList<ArrayList<Point>> list = new ArrayList<>();
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
 		
 		for(int i = 0; i <= V; i++) {
-			list.add(new ArrayList<Point>());
+			graph.add(new ArrayList<>());
 		}
 		
+		int K = Integer.parseInt(br.readLine());	// 시작 정점의 번호
 		for(int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 			
-			list.get(u).add(new Point(v, w));
+			graph.get(u).add(new Point(v, w));
 		}
 		
-		int [] result = bfs(list, V, K);
+		// 구현
+		dist = new int[V+1];
+		Arrays.fill(dist, Integer.MAX_VALUE);		//  결과 배열 최댓값으로 초기화
+		dijkstra(K);
 		
+		// 결과 출력 
 		StringBuilder sb = new StringBuilder();
 		for(int i = 1; i <= V; i++) {
-			if(result[i] == Integer.MAX_VALUE) sb.append("INF"+"\n");
-			else sb.append(result[i]+"\n");
+			if(dist[i] == Integer.MAX_VALUE)sb.append("INF\n");
+			else sb.append(dist[i]+"\n");
 		}
 		
 		System.out.println(sb.toString());
 
 	}
 	
-	static int [] bfs(ArrayList<ArrayList<Point>> list, int V, int K) {
-		int [] arr = new int[V+1];
-		Arrays.fill(arr,  Integer.MAX_VALUE);
-		arr[K] = 0;
-		
+	static void dijkstra(int start) {
 		PriorityQueue<Point> pq = new PriorityQueue<>();
-		pq.add(new Point(K, 0));
+		pq.add(new Point(start, 0));		// 시작점, 가중치
+		dist[start] = 0;	// 자기자신은 0
 		
 		while(!pq.isEmpty()) {
-			Point p = pq.poll();
+			Point point = pq.poll();
 			
-			if(arr[p.v] < p.w) continue;	// 이미 최솟값
+			if(dist[point.v] < point.w) continue;	// 이미 방문한 경우 -> 다음으로 넘어가기 
+
 			
-			for(int i = 0; i < list.get(p.v).size(); i++) {
-				int curV = list.get(p.v).get(i).v;
-				int curW = list.get(p.v).get(i).w;
+			List<Point> tmp = graph.get(point.v);
+			for(int i = 0; i <  tmp.size(); i++) {
+				int nextV = tmp.get(i).v;		// 인접 번호
+				int nextW = tmp.get(i).w;		// 해당 가중치
 				
-				if(arr[curV] > p.w + curW) {
-					arr[curV] = p.w + curW;
-					pq.add(new Point(curV, arr[curV]));
+				if(dist[point.v]+nextW < dist[nextV]) {
+					dist[nextV] = dist[point.v]+nextW;
+					pq.add(new Point(nextV, dist[nextV]));
 				}
 			}
 		}
-		
-		return arr;
 	}
 
 }
