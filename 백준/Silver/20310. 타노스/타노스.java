a1e6c3782c5a82db1d5e7ffbba2108dd558ff7e9
skyungg <167261107+skyungg@users.noreply.github.com>
2025-11-03 02:22:06 +0900

    [Silver III] Title: 타노스, Time: 104 ms, Memory: 14288 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -16,16 +16,27 @@ public class Main {
 			else cnt1++;
 		}
 		
-		cnt0/=2;
+		cnt0/=2;		// 각각 나온 개수의 절반 날리기
 		cnt1/=2;
 		
-		StringBuilder sb = new StringBuilder();
+		// 앞에서부터 1을 제거하며 남기기 -> 그래야 사전순에 가까워짐
+		StringBuilder sb1 = new StringBuilder();
+		for(int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if(c == '1' && cnt1 > 0) cnt1--;	// 해당 1 제거하기
+			else sb1.append(c);
+		}
 		
-		for(int i = 0; i < cnt0; i++) sb.append('0');
+		// 뒤에서부터 0 제거하기
+		StringBuilder sb2 = new StringBuilder();
+		for(int i = sb1.length()-1; i >= 0 ; i--) {
+			char c = sb1.charAt(i);
+			if(c == '0' && cnt0 > 0) cnt0--;
+			else sb2.append(c);
+			
+		}
 		
-		for(int i = 0; i < cnt1; i++) sb.append('1');
-		
-		System.out.println(sb);
+		System.out.println(sb2.reverse().toString());
 	}
 
 }
