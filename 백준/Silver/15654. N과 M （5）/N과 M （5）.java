c5b2fcb4f6f4bf34b42b827e9acbe6845fb3435e
skyungg <167261107+skyungg@users.noreply.github.com>
2025-11-15 23:41:58 +0900

    [Silver III] Title: N과 M (5), Time: 376 ms, Memory: 42380 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static int [] nums;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		nums = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		// 정렬
+		Arrays.sort(nums);
+		
+		dfs(0, new int[M], new boolean[N]);
+		
+		
+		// 출력
+		System.out.println(sb);
+	}
+	
+	static void dfs(int cnt, int [] arr, boolean [] visited) {
+		if(cnt == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(arr[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = 0; i < N; i++) {
+			if(visited[i]) continue;		// 이미 방문함
+			visited[i] = true;
+			arr[cnt] = nums[i];
+			dfs(cnt+1, arr, visited);
+			visited[i] = false;
+		}
+	}
+
+}
