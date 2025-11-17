ed2d717512ee7e4af264f74e259f6f02e10b4f88
skyungg <167261107+skyungg@users.noreply.github.com>
2025-11-17 22:05:09 +0900

    [Silver III] Title: N과 M (8), Time: 180 ms, Memory: 22236 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static int [] nums;
+	static int [] result;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		nums = new int[N];
+		result = new int[M];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		// 정렬
+		Arrays.sort(nums);
+		
+		dfs(0);
+		
+		// 정답 출력
+		System.out.println(sb);
+
+	}
+	
+	static void dfs(int cnt) {
+		if(cnt == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(result[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = 0; i < N; i++) {
+			if(cnt > 0 && nums[i] < result[cnt-1]) continue;
+			result[cnt] = nums[i];
+			dfs(cnt+1);
+		}
+	}
+}
