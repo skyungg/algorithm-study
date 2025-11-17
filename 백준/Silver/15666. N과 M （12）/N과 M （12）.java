7bf170887913286741dcc3e9f3b466fb81eb0b71
skyungg <167261107+skyungg@users.noreply.github.com>
2025-11-18 00:17:08 +0900

    [Silver II] Title: N과 M (12), Time: 144 ms, Memory: 16004 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static int [] nums;
+	static int [] result;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		nums = new int[N];
+		result = new int[M];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		// 정렬
+		Arrays.sort(nums);
+		
+		dfs(0, 0);
+		
+		// 정답 출력
+		System.out.println(sb);
+		
+	}
+	
+	static void dfs(int cnt, int idx) {
+		if(cnt == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(result[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		int prev = -1;
+		for(int i = idx; i < N; i++) {
+			if(prev == nums[i]) continue;
+			prev = nums[i];
+			result[cnt] = nums[i];
+			dfs(cnt+1, i);
+		}
+	}
+	
+	
+
+}
