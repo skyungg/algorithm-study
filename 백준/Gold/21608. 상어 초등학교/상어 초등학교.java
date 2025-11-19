ea505de8229d6072903d5eff781125ff2ab09318
skyungg <167261107+skyungg@users.noreply.github.com>
2025-11-19 20:40:17 +0900

    [Gold V] Title: 상어 초등학교, Time: 176 ms, Memory: 18972 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.java
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.java
@@ -2,35 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class Point implements Comparable<Point>{
-		int x;
-		int y;
-		int count;		// 좋아하는 학생수
-		int empty;		// 비어있는 칸
-		
-		public Point(int x, int y, int count, int empty) {
-			this.x = x;
-			this.y = y;
-			this.count = count;
-			this.empty = empty;
-		}
-		
-		/* 정렬
-		 * 1. 인접칸(4방향)에 좋아하는 학생 수가 더 많은 순
-		 * 2. 인접 칸 중 비어있는 칸이 가장 많은 순
-		 * 3. 행, 열  작은 순
-		 * */
-		@Override
-		public int compareTo(Point p) {
-			if(p.count == this.count) {
-				if(p.empty == this.empty) {
-					if(p.x == this.x) return this.y - p.y;
-					else return this.x - p.x;
-				}else return p.empty - this.empty;
-			}else return p.count - this.count;
-		}
-	}
-	
 	static int N;
 	static int [][] map;
 	static List<List<Integer>> list = new ArrayList<>();
@@ -62,7 +33,6 @@ public class Main {
 		
 		// 만족도 조사
 		int score = 0;
-//		int [] cost = {0, 1, 10, 100, 1000};
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
 				int target = map[i][j];		// 기준 학생
@@ -82,8 +52,6 @@ public class Main {
 					score += (int)Math.pow(10, (cnt-1));
 					
 				}
-				
-				
 			}
 		}
 		
@@ -119,28 +87,16 @@ public class Main {
 					
 					// 위치 갱신 판단하기
 					if(cnt > result[0]) {		// 좋아하는 학생수가 더 많음 -> 갱신 
-						result[0] = cnt;
-						result[1] = empty;
-						result[2] = i;
-						result[3] = j;
+						result = update(cnt, empty, i, j);
 					}else if(cnt == result[0]) {
 						if(empty > result[1]) {
-							result[0] = cnt;
-							result[1] = empty;
-							result[2] = i;
-							result[3] = j;
+							result = update(cnt, empty, i, j);
 						}else if(empty == result[1]) {
 							if(i < result[2]) {
-								result[0] = cnt;
-								result[1] = empty;
-								result[2] = i;
-								result[3] = j;
+								result = update(cnt, empty, i, j);
 							}else if(i == result[2]) {
 								if(j < result[3]) {
-									result[0] = cnt;
-									result[1] = empty;
-									result[2] = i;
-									result[3] = j;
+									result = update(cnt, empty, i, j);
 								}
 							}
 						}
@@ -158,5 +114,8 @@ public class Main {
 	static boolean checkRange(int x, int y) {
 		return x >= 0 && x < N && y >= 0 && y < N; 
 	}
-
+	
+	static int [] update(int cnt, int empty, int x, int y) {
+		return new int[] {cnt, empty, x, y};
+	}
 }
