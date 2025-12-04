f1873310133ea197ba82e833b414856043f9d810
skyungg <167261107+skyungg@users.noreply.github.com>
2025-12-05 00:06:56 +0900

    [Gold V] Title: 토마토, Time: 608 ms, Memory: 129100 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,86 +2,66 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class Point{
-		int x;
-		int y;
-		int time;
-		
-		public Point(int x, int y, int time){
-			this.x = x;
-			this.y = y;
-			this.time = time;
-		}
-		
-	}
-	
-	static int n, m;
+	static int N, M;
 	static int [][] map;
-	static ArrayList<int []> list = new ArrayList<>();
-	static Queue<Point> que = new LinkedList<>();
+	static boolean [][] visited;
+	static int [] dx = {-1, 0, 1, 0};
+	static int [] dy = {0, 1, 0, -1};
+	static int count = 0;		// 안 익은 토마토 개수
+	static Queue<int []> que = new LinkedList<>();
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		m = Integer.parseInt(st.nextToken());
-		n = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 		
-		map = new int[n][m];
-		for(int i = 0; i < n; i++) {
+		map = new int[N][M];
+		visited = new boolean[N][M];
+		
+		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < m; j++) {
+			for(int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] == 1) que.add(new Point(i, j, 0));
-				}
-		}
-		
-		
-		// 1. 토마토 후숙
-		int result = bfs();
-		
-		// 2. 토마토 결과
-		boolean flag = true;
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++) {
-				if(map[i][j] == 0) {
-					
-					flag = false;
-					break;
-				}
+				if(map[i][j] == 0) count++;
+				if(map[i][j] == 1) que.add(new int[] {i, j, 0});
 			}
-			if(!flag) break;
 		}
 		
-		if(!flag) System.out.println(-1);
-		else System.out.println(result);
+		// 정답 출력
+		
+		if(count == 0) System.out.println(0);
+		else System.out.println(bfs());
+		
 	}
 	
 	static int bfs() {
-		int dx [] = {-1, 0, 1, 0};
-		int dy [] = {0, 1, 0, -1};
-		
-		boolean [][] visited = new boolean[n][m];
-		
 		int result = 0;
 		while(!que.isEmpty()) {
-			Point p = que.poll();
-			visited[p.x][p.y] = true;
+			if(count == 0) break;
 			
-			result = Math.max(result,  p.time);
+			int [] point = que.poll();
 			
 			for(int i = 0; i < 4; i++) {
-				int tx = p.x + dx[i];
-				int ty = p.y + dy[i];
+				int tx = point[0] + dx[i];
+				int ty = point[1] + dy[i];
 				
-				if(tx >= 0 && tx < n && ty >= 0 && ty < m) {
-					if(map[tx][ty] == 0 && !visited[tx][ty]) {
-						map[tx][ty] = 1;
-						que.add(new Point(tx, ty, p.time+1));
-					}
+				if(!checkRange(tx, ty)) continue;
+				if(map[tx][ty] == 0 && !visited[tx][ty]) {
+					visited[tx][ty] = true;
+					result = Math.max(result, point[2]+1);
+					que.add(new int [] {tx, ty, point[2]+1});
+					count--;
 				}
 			}
 		}
-		return result;
+		
+		if(count > 0) return -1;
+		else return result;
+	}
+	
+	static boolean checkRange(int x, int y) {
+		return x >= 0 && x < N && y >= 0 && y < M;
 	}
 
 }
