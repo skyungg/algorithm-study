8068d7a060d2a2ec3ea8b580ed1ec979220130f4
skyungg <167261107+skyungg@users.noreply.github.com>
2025-12-12 00:20:39 +0900

    [Silver I] Title: 스타트링크, Time: 184 ms, Memory: 62896 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -1,48 +1,55 @@
 import java.io.*;
 import java.util.*;
 
+/*
+ * 아이디어: bfs
+ * 힌트: 최솟값을 출력 -> bfs
+ * 구: S층에서 G층으로 가기 위해 눌러야 하는 버튼의 수의 최솟값을 출력
+ * */
 public class Main {
-    public static void main(String [] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int f = Integer.parseInt(st.nextToken());       // 총 층
-        int s = Integer.parseInt(st.nextToken());       // 시작 층
-        int g = Integer.parseInt(st.nextToken());       // 목표 층
-        int u = Integer.parseInt(st.nextToken());       // 한 번에 올라가는 층
-        int d = Integer.parseInt(st.nextToken());       // 한 번에 내려가는 층
+	static int F;
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		F = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());	// 현재 위치
+		int G = Integer.parseInt(st.nextToken());	// 도착 위치
+		int [] steps = {Integer.parseInt(st.nextToken()), -Integer.parseInt(st.nextToken())};
+		
+		boolean [] visited = new boolean[F+1];
+		Queue<int []> que = new LinkedList<>();
+		que.add(new int [] {S, 0});	// 현재 위치, COUNT
+		visited[S] = true;
+		int result = Integer.MAX_VALUE;
+		
+		while(!que.isEmpty()) {
+			int [] point = que.poll();
+			if(point[0] == G) {
+				result = point[1];
+				break;
+			}
+			
+			// 조작하기
+			for(int i = 0; i < 2; i++) {
+				int s = point[0] + steps[i];
+				if(!checkRange(s)) continue;
+				if(visited[s]) continue;
+				
+				visited[s] = true;
+				que.add(new int[] {s, point[1]+1});
+			}
+		}
+		
+		// 출력
+		if(result == Integer.MAX_VALUE)System.out.println("use the stairs");
+		else System.out.println(result);
+		
+	}
+	
+	static boolean checkRange(int cur) {
+		return cur >= 1 && cur <= F;
+	}
 
-        if(s == g){
-            System.out.println(0);
-            return;
-        }
-
-        Queue<Integer> que = new LinkedList<>();
-        que.add(s);
-        int [] tmp = new int[2];
-        int [] visited = new int[f+1];
-
-        int count = -1;
-        visited[s] = 1;
-        while(!que.isEmpty()){
-            int point = que.poll();
-
-            if(point == g){
-                count = visited[g] - 1;
-                break;
-            }
-
-            tmp[0] = point+u;
-            tmp[1] = point-d;
-
-            for(int i = 0; i < 2; i++){
-                if(tmp[i] > 0 && tmp[i] <= f && visited[tmp[i]] == 0){
-                    visited[tmp[i]] = visited[point] + 1;
-                    que.add(tmp[i]);
-                }
-            }
-        }
-
-        if(count != -1)System.out.println(count);
-        else System.out.println("use the stairs");
-    }
 }
